dbca94688fbc2e1d2b3aaf55b64022cb4b1d16ff
Anna <yorocobe@gmail.com>
2025-01-27 11:49:36 +0900

    feat: 매핑 경로를 사용하기 위해 webpack config에 path 관련 옵션 추가

--- gatsby-node.js
+++ gatsby-node.js
@@ -123,3 +123,16 @@ exports.createSchemaCustomization = ({ actions }) => {
     }
   `)
 }
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+  })
+}
